9b27f3e614ffedf9c6eb83bb3a40f8c404d22d57
Pranav Srivastav <109237040+iamransh@users.noreply.github.com>
2023-10-06 22:13:26 +0530

    initial commit 2

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,7 +5,7 @@ import { ChakraProvider } from "@chakra-ui/react";
 export default function App({ Component, pageProps }) {
   return (
     <ChakraProvider>
-      <TheRestOfYourApplication />
+      <Component {...pageProps} />
     </ChakraProvider>
   );
 }
