d3f9c575b0c5e6f9d810874170c32012f96a950d
Pranav Srivastav <109237040+iamransh@users.noreply.github.com>
2023-10-07 11:55:02 +0530

    Added portfolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Flex, Text, Stack, Image, Box, Button } from "@chakra-ui/react";
+import Slider from "react-slick";
+import img1 from "../assets/train_color.jpg";
+import img2 from "../assets/train_base.jpg";
 
 export default function Portfolio() {
+  const slide = useRef(null);
+  const settings = {
+    autoplay: false,
+    dots: false,
+    infinite: true,
+    arrows: false,
+    speed: 3000,
+    autoplaySpeed: 6000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
   return (
     <Stack w="100%" py="100px" bg="white" gap={5} id="portfolio" zIndex={100}>
       <Text textAlign={"center"} fontWeight={"light"} fontSize={"2.5em"}>
@@ -9,6 +23,28 @@ export default function Portfolio() {
       </Text>
       <Flex w="100%" justify={"center"}>
         <Flex w="50px" h="3px" bg="black"></Flex>
+      </Flex>
+      <Flex w="100%" justify={"center"} mt="10px">
+        <Stack w={["95vw", "80vw", "60vw"]}>
+          <Slider ref={slide} {...settings}>
+            <div>
+              <Image
+                src={img1.src}
+                alt="img1"
+                w={["95vw", "80vw", "60vw"]}
+                h={["80vw", "60vw", "40vw"]}
+              />
+            </div>
+            <div>
+              <Image
+                src={img2.src}
+                alt="img2"
+                w={["95vw", "80vw", "60vw"]}
+                h={["80vw", "60vw", "40vw"]}
+              />
+            </div>
+          </Slider>
+        </Stack>
       </Flex>
     </Stack>
   );
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,8 @@
 import "@/styles/globals.css";
 import * as React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export default function App({ Component, pageProps }) {
   return (
